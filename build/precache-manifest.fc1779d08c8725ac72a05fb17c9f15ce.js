9995b700d7bf168f8ee19c09d3e3382095f3e57a
apple <apple@192.168.0.105>
2022-11-04 14:45:35 +0500

    Build 4-11-2022

---
+++ build/precache-manifest.fc1779d08c8725ac72a05fb17c9f15ce.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d279c845baf031f29409b6127e1c5318",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffda18db69e9ce89bc35",
+    "url": "/static/css/2.2d917472.chunk.css"
+  },
+  {
+    "revision": "6f1c21037891dcc5c78f",
+    "url": "/static/css/main.184168ea.chunk.css"
+  },
+  {
+    "revision": "ffda18db69e9ce89bc35",
+    "url": "/static/js/2.de700860.chunk.js"
+  },
+  {
+    "revision": "85af2cee83e68ad936762bf229f48288",
+    "url": "/static/js/2.de700860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1c21037891dcc5c78f",
+    "url": "/static/js/main.47c8684f.chunk.js"
+  },
+  {
+    "revision": "ed4b55a0e08fb71dd5a7",
+    "url": "/static/js/runtime-main.a4ffef54.js"
+  },
+  {
+    "revision": "9f0f13ca293693748460d9a009fe1a50",
+    "url": "/static/media/AutomationTestingProcess-1.9f0f13ca.png"
+  },
+  {
+    "revision": "8d38015300b1ba692397129628958a72",
+    "url": "/static/media/TRYVesterbro-Light.8d380153.otf"
+  },
+  {
+    "revision": "4f98164c9c8cc43b2d5b808a8766b479",
+    "url": "/static/media/TRYVesterbro-Medium.4f98164c.otf"
+  },
+  {
+    "revision": "fe478a604ff6ef4de4031ad5f079a3e9",
+    "url": "/static/media/TRYVesterbro-Poster.fe478a60.otf"
+  },
+  {
+    "revision": "9f1600e9e894cb0ebeff10093753d9ad",
+    "url": "/static/media/ajeeth.9f1600e9.png"
+  },
+  {
+    "revision": "47909bc3ab498fb83fddc90a77232f6b",
+    "url": "/static/media/alprints.47909bc3.jpg"
+  },
+  {
+    "revision": "e46ddda569b54c5b1bc4014ef8fb69cf",
+    "url": "/static/media/alprints.e46ddda5.png"
+  },
+  {
+    "revision": "197bc47344b60022a0a2392058810dd2",
+    "url": "/static/media/applications.197bc473.png"
+  },
+  {
+    "revision": "907633e5c050d25c9e312c196ed5418c",
+    "url": "/static/media/b1.907633e5.jpg"
+  },
+  {
+    "revision": "1a2b729787981871839eea4af14bbee7",
+    "url": "/static/media/bootstrap-logo.1a2b7297.png"
+  },
+  {
+    "revision": "3740e78944f190c4b8513a97ae6bac15",
+    "url": "/static/media/bootstrap.3740e789.png"
+  },
+  {
+    "revision": "3e3000d936fc6afa47a41c266bacf2ba",
+    "url": "/static/media/cmobiler.3e3000d9.png"
+  },
+  {
+    "revision": "44365a738f4865ee00b6b129ed715c74",
+    "url": "/static/media/cybros-logo-i.44365a73.png"
+  },
+  {
+    "revision": "af528128cf97404ef26bdba9d2ff3ca0",
+    "url": "/static/media/digiart.af528128.png"
+  },
+  {
+    "revision": "18af29b5c61e3f9ab416589c547bffd7",
+    "url": "/static/media/django.18af29b5.svg"
+  },
+  {
+    "revision": "0949260eb20f6caa82497bfe508f1e19",
+    "url": "/static/media/doc-wizard.0949260e.png"
+  },
+  {
+    "revision": "e5429fa70a87c4db3dba97b4399cca8a",
+    "url": "/static/media/eltcloud.e5429fa7.svg"
+  },
+  {
+    "revision": "0644126c18dd1924f65267d5635042f1",
+    "url": "/static/media/estrella-logo.0644126c.avif"
+  },
+  {
+    "revision": "e4b2429a61f42db5b546374e3c8f4367",
+    "url": "/static/media/fig-testing-process.e4b2429a.svg"
+  },
+  {
+    "revision": "1cc9604eb9f124a4261afb5148a80db7",
+    "url": "/static/media/fly.1cc9604e.svg"
+  },
+  {
+    "revision": "c1ef81f54b894f7852383b478be81ef1",
+    "url": "/static/media/getaroom.c1ef81f5.svg"
+  },
+  {
+    "revision": "268d07772e674f7727b22d43feea87cd",
+    "url": "/static/media/gql.268d0777.svg"
+  },
+  {
+    "revision": "8c52471d68fbaba8093a0132ae0e20c6",
+    "url": "/static/media/html5.8c52471d.png"
+  },
+  {
+    "revision": "0b7458bff12518139e94ea9908cb3cad",
+    "url": "/static/media/iPad-Application-meridian-uae.0b7458bf.jpg"
+  },
+  {
+    "revision": "870ac530ba4fae95393af8784c404f71",
+    "url": "/static/media/intimely.870ac530.webp"
+  },
+  {
+    "revision": "5a8b5c70a529c0156d8b1ce3ed7ba614",
+    "url": "/static/media/laravel.min.5a8b5c70.svg"
+  },
+  {
+    "revision": "cb2f65a2181811ba82d3db0ed0880d1c",
+    "url": "/static/media/material.cb2f65a2.svg"
+  },
+  {
+    "revision": "af4f8e40860c631dd20d452132e50898",
+    "url": "/static/media/morph.af4f8e40.svg"
+  },
+  {
+    "revision": "d8e2b155344f7acfeeefce56c454aaa7",
+    "url": "/static/media/nodejs.d8e2b155.png"
+  },
+  {
+    "revision": "7f2f0d997e5ea0430ff8afd068c22b94",
+    "url": "/static/media/php-logo.7f2f0d99.png"
+  },
+  {
+    "revision": "4b87b61f1859f6f9b2f8cf15c4a8ca0d",
+    "url": "/static/media/pngfind.com-wtf-png-112707.4b87b61f.png"
+  },
+  {
+    "revision": "ff4ae885705bf030392f8c31c1c8845d",
+    "url": "/static/media/rails.ff4ae885.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "eb299d0a4986657efa5e99c04a3b35e2",
+    "url": "/static/media/responsive-design.eb299d0a.jpg"
+  },
+  {
+    "revision": "8bddded8abce11ffe34185d06faa4339",
+    "url": "/static/media/security.8bddded8.jpg"
+  },
+  {
+    "revision": "0c4442e6358c83661d7603b04b6aac84",
+    "url": "/static/media/seo.0c4442e6.svg"
+  },
+  {
+    "revision": "042c3d7ebd751120815d530961e9a2cc",
+    "url": "/static/media/shopify.042c3d7e.svg"
+  },
+  {
+    "revision": "65b0e9c86f756a5fdbd7123f2a3cc02e",
+    "url": "/static/media/sis.65b0e9c8.svg"
+  },
+  {
+    "revision": "fc994caef4476437992981e8d06d8a8a",
+    "url": "/static/media/sis.fc994cae.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "f5f02e3dbefab99b68d52b39b4d53cd8",
+    "url": "/static/media/storybook.f5f02e3d.png"
+  },
+  {
+    "revision": "d4cf9db60cb99d8e3a88a8b83f9efdd1",
+    "url": "/static/media/t-1.d4cf9db6.jpg"
+  },
+  {
+    "revision": "47f7b6a85d482193e0d290c764b49b30",
+    "url": "/static/media/tech.47f7b6a8.jpeg"
+  },
+  {
+    "revision": "7540fedc12de6ec6e2f95b5bfbeb73a9",
+    "url": "/static/media/testcafe.7540fedc.png"
+  },
+  {
+    "revision": "7f987da2721ac8bbbbef13faf1611f95",
+    "url": "/static/media/testsInc.7f987da2.png"
+  },
+  {
+    "revision": "680d5511e340bcdcfa23cbe664a5c32a",
+    "url": "/static/media/tripbuddyapp.680d5511.png"
+  },
+  {
+    "revision": "1dfbb7dc05b3f61a817874215a37152c",
+    "url": "/static/media/umbrella_logo_white_letters_small_blue.1dfbb7dc.png"
+  },
+  {
+    "revision": "6eaf0605a5cae5d8359ccc91b9c4695a",
+    "url": "/static/media/vue.6eaf0605.jpg"
+  },
+  {
+    "revision": "1c04faafdee0cb906902f8ca169c89ff",
+    "url": "/static/media/web-development.1c04faaf.png"
+  },
+  {
+    "revision": "0ca4bf5c73ba655f3c75b3831f90b5ad",
+    "url": "/static/media/web.0ca4bf5c.jpg"
+  },
+  {
+    "revision": "c88557eac253581f859bdbafe75b662e",
+    "url": "/static/media/webdesign.c88557ea.jpg"
+  },
+  {
+    "revision": "b4758e597fc40675ecec3a9657e0836c",
+    "url": "/static/media/webdev.b4758e59.jpg"
+  },
+  {
+    "revision": "8531a95c64fa10b870c32f42e25fd87c",
+    "url": "/static/media/wordpress.8531a95c.svg"
+  },
+  {
+    "revision": "f02ed005482a2aeb1bc291df9c31a9a6",
+    "url": "/static/media/write.f02ed005.jpg"
+  }
+]);
